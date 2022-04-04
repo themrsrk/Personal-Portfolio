a30e122352132302e444676af9eb7134ab8553dd
Shahrukh <themrsrk@gmail.com>
2022-04-04 11:09:17 +0500

    Update Projects & Acheivements

--- src/data/achievementData.js
+++ src/data/achievementData.js
@@ -1,29 +1,32 @@
+import pirple from "../assets/images/pirple.png"
+import open from "../assets/images/open.png"
+import edx from "../assets/images/edx.webp"
 export const achievementData = {
     bio : "Putting quantified achievements on a resume is great, but I just don’t work with hard numbers. I have no data to support the quality of my work. If you have lots of relevant experience, the heading statement of your resume will be a summary.",
     achievements : [
         {
             id : 1,
-            title : 'Google Cloud certification',
-            details : 'Google Cloud certifications validate your expertise and showcase your ability to transform businesses with Google Cloud technology.',
-            date : 'Aug 20, 2019',
-            field : 'Automation',
-            image : 'https://images.unsplash.com/photo-1573141597928-403fcee0e056?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=667&q=80'
+            title : 'Intro to Server Administration',
+            details : `Gain Skills that how to setup and operate your own virtual servers on Digital Ocean. Also include Provisioning, SSH, Putty, the FileSystem, Permissions, Nano, Vim, Package Management with Yum, LAMP Stack configuration, Integrating Git, Operating a Database, DNS, Virtual Hosts, Firewalls, Snapshots & Logging as well.`,
+            date : 'April 3, 2022',
+            field : 'DevOps',
+            image : pirple
         },
         {
             id : 2,
-            title : 'AWS Certification',
-            details : 'Validate technical skills and cloud expertise to grow your career and business.',
-            date : 'Aug 20, 2019',
-            field : 'Automation',
-            image : 'https://images.unsplash.com/photo-1523474253046-8cd2748b5fd2?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80'
+            title : 'Programming Essentials in Python',
+            details : 'Knows the universal concepts of computer programming, including variables, data structures, algorithms, control flow, functions, and exceptions',
+            date : 'Mar 16, 2022',
+            field : 'Programming',
+            image : open
         },
         {
             id : 3,
-            title : 'Professional Data Engineer Certification',
-            details : 'Professional Data Engineers enable data-driven decision making by collecting, transforming, and publishing data.',
-            date : 'Aug 20, 2019',
+            title : 'Automated Software Testing: Unit Testing, Coverage Criteria and Design for Testability',
+            details : 'Gain valuable software testing techniques, tools, and best practices required to deliver high-quality software.',
+            date : 'Dec 21, 2021',
             field : 'Automation',
-            image : 'https://images.unsplash.com/photo-1489389944381-3471b5b30f04?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80'
+            image : edx
         }
 
     ]
--- src/data/projectsData.js
+++ src/data/projectsData.js
@@ -11,76 +11,76 @@ import eight from '../assets/svg/projects/eight.svg'
 export const projectsData = [
     {
         id: 1,
-        projectName: 'Employee Management',
-        projectDesc: 'This project aims to build an employee management system that consolidates all information of a company',
-        tags: ['React', 'CSS', 'Material Ui'],
-        code: 'https://github.com/hhhrrrttt222111/developer-portfolio',
-        demo: 'https://github.com/hhhrrrttt222111/developer-portfolio',
+        projectName: 'Wind1 News Web App',
+        projectDesc: 'This project aims to build a worldwide news app, which let the people up to date from latest news. ',
+        tags: ['React', 'CSS', 'HTML'],
+        code: 'https://github.com/themrsrk?tab=repositories',
+        demo: 'https://github.com/themrsrk?tab=repositories',
         image: one
     },
     {
         id: 2,
-        projectName: 'Railway Tracking',
-        projectDesc: 'This project is a railway tracking system that allows users to obtain relevant information about train timing.',
-        tags: ['Django', 'Tailwind CSS'],
-        code: 'https://github.com/hhhrrrttt222111/developer-portfolio',
-        demo: 'https://github.com/hhhrrrttt222111/developer-portfolio',
+        projectName: 'Pluto Notebook',
+        projectDesc: 'This project is an online note book that allows users to create their own notes in their on account.',
+        tags: ['React', 'HTML, CSS'],
+        code: 'https://github.com/themrsrk?tab=repositories',
+        demo: 'https://github.com/themrsrk?tab=repositories',
         image: two
     },
     {
         id: 3,
-        projectName: 'Weather App',
-        projectDesc: 'Weather forecast systems and applications predict weather conditions based on multiple parameters.',
-        tags: ['Django', 'CSS', 'Material Ui'],
-        code: 'https://github.com/hhhrrrttt222111/developer-portfolio',
-        demo: 'https://github.com/hhhrrrttt222111/developer-portfolio',
+        projectName: 'Flutter Catalog App',
+        projectDesc: 'This is a simple mobile app with a simple navigation drawer',
+        tags: ['Flutter Dart'],
+        code: 'https://github.com/themrsrk?tab=repositories',
+        demo: 'https://github.com/themrsrk?tab=repositories',
         image: three
     },
-    {
-        id: 4,
-        projectName: 'Android Patient Tracker',
-        projectDesc: 'This project involves the development of an Android application for viewing and managing patient data.',
-        tags: ['Flutter', 'Firebase'],
-        code: 'https://github.com/hhhrrrttt222111/developer-portfolio',
-        demo: 'https://github.com/hhhrrrttt222111/developer-portfolio',
-        image: four
-    },
-    {
-        id: 5,
-        projectName: 'E-Commerce App',
-        projectDesc: 'A Simple E-commerce application',
-        tags: ['React Native', 'Firebase'],
-        code: 'https://github.com/hhhrrrttt222111/developer-portfolio',
-        demo: 'https://github.com/hhhrrrttt222111/developer-portfolio',
-        image: five
-    },
-    {
-        id: 6,
-        projectName: 'Uber Lite',
-        projectDesc: 'Uber clone',
-        tags: ['Flutter'],
-        code: 'https://github.com/hhhrrrttt222111/developer-portfolio',
-        demo: 'https://github.com/hhhrrrttt222111/developer-portfolio',
-        image: six
-    },
-    {
-        id: 7,
-        projectName: 'Stock Market App',
-        projectDesc: 'A simple stock market API app',
-        tags: ['React', 'Redux', 'Bootstrap'],
-        code: 'https://github.com/hhhrrrttt222111/developer-portfolio',
-        demo: 'https://github.com/hhhrrrttt222111/developer-portfolio',
-        image: seven
-    },
-    {
-        id: 8,
-        projectName: 'Car Pooling System',
-        projectDesc: 'The carpooling system merges multiple people in a car which leads to meet new people, reduces pollution',
-        tags: ['Flutter', 'React'],
-        code: 'https://github.com/hhhrrrttt222111/developer-portfolio',
-        demo: 'https://github.com/hhhrrrttt222111/developer-portfolio',
-        image: eight
-    },
+    // {
+    //     id: 4,
+    //     projectName: 'Android Patient Tracker',
+    //     projectDesc: 'This project involves the development of an Android application for viewing and managing patient data.',
+    //     tags: ['Flutter', 'Firebase'],
+    //     code: 'https://github.com/hhhrrrttt222111/developer-portfolio',
+    //     demo: 'https://github.com/hhhrrrttt222111/developer-portfolio',
+    //     image: four
+    // },
+    // {
+    //     id: 5,
+    //     projectName: 'E-Commerce App',
+    //     projectDesc: 'A Simple E-commerce application',
+    //     tags: ['React Native', 'Firebase'],
+    //     code: 'https://github.com/hhhrrrttt222111/developer-portfolio',
+    //     demo: 'https://github.com/hhhrrrttt222111/developer-portfolio',
+    //     image: five
+    // },
+    // {
+    //     id: 6,
+    //     projectName: 'Uber Lite',
+    //     projectDesc: 'Uber clone',
+    //     tags: ['Flutter'],
+    //     code: 'https://github.com/hhhrrrttt222111/developer-portfolio',
+    //     demo: 'https://github.com/hhhrrrttt222111/developer-portfolio',
+    //     image: six
+    // },
+    // {
+    //     id: 7,
+    //     projectName: 'Stock Market App',
+    //     projectDesc: 'A simple stock market API app',
+    //     tags: ['React', 'Redux', 'Bootstrap'],
+    //     code: 'https://github.com/hhhrrrttt222111/developer-portfolio',
+    //     demo: 'https://github.com/hhhrrrttt222111/developer-portfolio',
+    //     image: seven
+    // },
+    // {
+    //     id: 8,
+    //     projectName: 'Car Pooling System',
+    //     projectDesc: 'The carpooling system merges multiple people in a car which leads to meet new people, reduces pollution',
+    //     tags: ['Flutter', 'React'],
+    //     code: 'https://github.com/hhhrrrttt222111/developer-portfolio',
+    //     demo: 'https://github.com/hhhrrrttt222111/developer-portfolio',
+    //     image: eight
+    // },
 ]
 
 
