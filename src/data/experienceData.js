684d4827fe4e4bcc22e0a07e958af72cb76224e7
Shahrukh <themrsrk@gmail.com>
2022-04-18 02:11:26 +0500

    Updates in Experience Data

--- src/data/experienceData.js
+++ src/data/experienceData.js
@@ -3,21 +3,21 @@ export const experienceData = [
         id: 1,
         company: 'wind1 ISP',
         jobtitle: 'Support & Operations Engineer',
-        startYear: '2021',
+        startYear: 'Oct 2021',
         endYear: 'Present'
     },
     {
         id: 2,
-        company: 'Prozamneem.com',
+        company: 'Prozameem.com',
         jobtitle: 'Frontend Developer',
-        startYear: 'May-2021',
-        endYear: 'Jul-2021'
+        startYear: 'May 2021',
+        endYear: 'Jul 2021'
     },
     {
         id: 3,
         company: 'IBA Academy Hyderabad',
         jobtitle: 'Mathematics Teacher',
-        startYear: 'Jan-2019',
-        endYear: 'Sep-2020'
+        startYear: 'Jan 2019',
+        endYear: 'Sep 2020'
     },
 ]
